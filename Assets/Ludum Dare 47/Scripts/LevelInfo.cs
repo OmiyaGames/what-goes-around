7314c86f92503568c156f15d29dc3edbe1eeb2fb
Taro Omiya <taro.omiya@omiyagames.com>
2020-10-03 12:43:57 -0400

    Creating the first level, and testing ship controls.

---
+++ Assets/Ludum Dare 47/Scripts/LevelInfo.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LudumDare47
+{
+    public class LevelInfo : MonoBehaviour
+    {
+        [SerializeField]
+        Transform center;
+
+        /// <summary>
+        /// Get the rotation info
+        /// </summary>
+        /// <param name="worldPosition"></param>
+        /// <returns></returns>
+        public Quaternion GetOrientation(Vector3 worldPosition)
+        {
+            return Quaternion.FromToRotation(Vector3.forward, GetGravityDirection(worldPosition));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <returns></returns>
+        public Quaternion GetOrientation(Transform transform)
+        {
+            return GetOrientation(transform.position);
+        }
+
+        /// <summary>
+        /// The direction for gravity to affect a location
+        /// </summary>
+        /// <param name="worldPosition"></param>
+        /// <returns></returns>
+        public Vector3 GetGravityDirection(Vector3 worldPosition)
+        {
+            Vector3 returnDirection = center.position - worldPosition;
+            returnDirection.Normalize();
+            return returnDirection;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <returns></returns>
+        public Vector3 GetGravityDirection(Transform transform)
+        {
+            return GetGravityDirection(transform.position);
+        }
+    }
+}
---
+++ Assets/Ludum Dare 47/Scripts/PlayerShip.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LudumDare47
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerShip : MonoBehaviour
+    {
+        [SerializeField]
+        LevelInfo info;
+
+        [Header("Physics")]
+        [SerializeField]
+        Rigidbody body;
+        [SerializeField]
+        float gravityStrength = 1000f;
+
+        [Header("Controls")]
+        [SerializeField]
+        float moveSpeed = 100f;
+        [SerializeField]
+        float inputSmooth = 10f;
+
+        public Quaternion Orientation
+        {
+            get;
+            private set;
+        } = Quaternion.identity;
+
+        Vector2 rawInput, finalInput;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            rawInput.x = Input.GetAxis("Horizontal");
+            rawInput.y = Input.GetAxis("Vertical");
+            rawInput.Normalize();
+
+            // Look into other ways to smooth from last input to new one
+            finalInput = Vector2.Lerp(finalInput, rawInput, (Time.deltaTime * inputSmooth));
+        }
+
+        private void FixedUpdate()
+        {
+            // First, update the rotation of this ship
+            Orientation = info.GetOrientation(transform);
+            body.MoveRotation(Orientation);
+
+            // Apply gravity
+            ApplyForce((Vector3.forward * gravityStrength), ForceMode.Acceleration);
+
+            // Apply movement
+            ApplyForce((finalInput * moveSpeed), ForceMode.VelocityChange);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Vector3 direction = Orientation * Vector3.right;
+            Gizmos.DrawLine(transform.position, (transform.position + direction));
+
+            Gizmos.color = Color.green;
+            direction = Orientation * Vector3.up;
+            Gizmos.DrawLine(transform.position, (transform.position + direction));
+        }
+
+        private void ApplyForce(Vector3 direction, ForceMode mode)
+        {
+            body.AddForce((Orientation * (direction * Time.deltaTime)), mode);
+        }
+
+        private void Reset()
+        {
+            body = GetComponent<Rigidbody>();
+        }
+    }
+}
